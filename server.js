65da8dd93dc8015394b7b7dd55f4fe812c547119
rramosx11 <rodolforamosd11@gmail.com>
2022-08-20 15:36:14 -0400

    added the database within the server

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require("mysql2");
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // Your mySQL username,
+    user: "root",
+    // Your mysql password
+    password: "",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
 
 // Default response for any request (Not Found)
 app.use((req, res) => {
